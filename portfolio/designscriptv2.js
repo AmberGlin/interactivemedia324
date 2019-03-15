1dc9c0c6771eaf70d5cf8b51330b1d79f61c51d0
Amber Glinski <aglinski16@wou.edu>
2019-03-14 23:43:50 -0700

    etqaettrnqe

---
+++ portfolio/designscriptv2.js
@@ -0,0 +1,15 @@
+// $(document).ready(
+//     function(){
+//         $(".gt").hover(function () {
+//             $("#light").toggle();
+//         });
+
+//         $(".ex").hover(function () {
+//             $("#eximg").toggle();
+//         });
+
+//         $(".ob").hover(function () {
+//             $("#OB").toggle();
+//         });
+
+//     });
